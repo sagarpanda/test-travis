919876bcc54340f210f3176796bda5a6fff31f60
Sagar Panda <er.sagarpanda@gmail.com>
2017-12-03 15:29:51 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,24 @@ setup_git() {
 upload_files() {
   # git remote add origin-pages https://${GH_TOKEN}@github.com/sagarpanda/test-travis.git > /dev/null 2>&1
   # git push --quiet --set-upstream origin-pages gh-pages
-  git remote add origin-pages https://${GH_TOKEN}@github.com/sagarpanda/test-travis.git > /dev/null 2>&1
+  #git remote add origin-pages https://${GH_TOKEN}@github.com/sagarpanda/test-travis.git > /dev/null 2>&1
   git subtree split --branch gh-pages --prefix dist
-  git push -f origin-pages gh-pages:gh-pages
+  git checkout gh-pages
+
+  cd dist
+  cp -r . ../
+  cd ..
+  rm -rf node_modules
+  rm -rf dist
+
+  git add -A
+  git commit -m "Travis build: 0"
+
+  #git push -f origin-pages gh-pages:gh-pages
+  git push -f origin gh-pages:gh-pages
+  git checkout master
   git branch -D gh-pages
 }
 
-setup_git
+#setup_git
 upload_files
